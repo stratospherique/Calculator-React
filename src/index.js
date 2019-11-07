61df51e6ff5a1a74974a6140131b6801dbd5db08
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-11-08 00:14:42 +0100

    heroku commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+const element = document.createElement('p');
+element.innerText = 'Hello world';
+document.querySelector('body').append(element);
