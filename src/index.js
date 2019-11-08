16498447b0e324c7f814b66cb7f5de159de7d433
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-11-08 20:15:46 +0100

    Add components but it's not showing any results :(

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './components/App';
 
-const element = document.createElement('p');
-element.innerText = 'Hello world';
-document.querySelector('body').append(element);
+const element = document.createElement('div');
+element.setAttribute('id', 'root');
+
+ReactDOM.render(
+  <App />,
+  element,
+)
